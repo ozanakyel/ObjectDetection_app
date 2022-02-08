fde540ef580a7cf96f5baafda8bc7e26a33be9bf
Harun <harun0836@gmail.com>
2022-02-08 14:45:51 +0300

    .

--- SafetyZone/models.py
+++ SafetyZone/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 class Project(models.Model):
     projectID = models.AutoField(primary_key=True)
+    plant = models.IntegerField(null=True)
+    name = models.CharField(max_length=200)
+    cameraIP = models.CharField(max_length=200)
+    cameraType = models.CharField(max_length=200)
+    userName = models.CharField(max_length=200)
+    userPassword = models.CharField(max_length=200)
+
+class Config(models.Model):
+    configKeyID = models.AutoField(primary_key=True)
+    configKey = models.CharField(max_length=200)
+
+class ProjectConfig(models.Model):
+    projectID = models.ForeignKey(Project, default= None, on_delete=models.CASCADE)
+    configID = models.ForeignKey(Project, default= None, on_delete=models.CASCADE)
+    configValue = models.CharField(max_length=200)
