606347a5eb0514f72566ccccd93eb58a501eef6a
Harun <harun0836@gmail.com>
2022-02-08 13:13:42 +0300

    .

--- objectdetectionfrontend/src/components/Home/Homepage.vue
+++ objectdetectionfrontend/src/components/Home/Homepage.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="middle">
     <div class="card">
-      <div v-for="n in 4" class="cards" :key="n">
+      <div v-for="n in 4" class="cards" @click="goToProject($event)" :key="n">
         <div class="cards-inside">
-          <div @click="goProject" class="header">
+          <div class="header">
             <h3>OBJECT DETECTION</h3>
             <div class="span">Project Active<span style="position: absolute;background-color: green;width: 8px;height: 8px;border-radius: 100%;margin-left: 5px;margin-top: 5px;"></span></div>
           </div>
-          <div @click="goProject" class="body">
+          <div class="body">
             <div>
               <div><p>Bypass Active<span style="color: green;">&#10003;</span></p></div>
               <div><p>Process Result</p></div>
@@ -18,7 +18,7 @@
           </div>
           <div class="run-stop">
             <div class="change">
-              <p>RUN</p>
+              <p class="change-text">RUN</p>
             </div>
           </div>
         </div>
@@ -41,10 +41,12 @@ export default {
     }
   },
   methods: {
-    goProject () {
-      this.$router.push({
-        name: 'Project'
-      })
+    goToProject (event) {
+      if (event.target.className !== 'change-text') {
+        this.$router.push({
+          name: 'Project'
+        })
+      }
     }
   },
   mounted () {
