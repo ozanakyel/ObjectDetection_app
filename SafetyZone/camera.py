12da20001ca0085215676aa6484c0e1ecde18772
Harun <harun0836@gmail.com>
2022-02-14 09:34:47 +0300

    .

--- SafetyZone/camera.py
+++ SafetyZone/camera.py
@@ -4,6 +4,7 @@ import numpy as np
 from django.conf import settings
 from SafetyZone import object_detector
 # from SafetyZone.object_detection import object_detection_folder_onnxTF2
+from .isIn import check_rois
 import logging
 from threading import Thread
 import time
@@ -17,11 +18,11 @@ from SafetyZone import rois
 
 from SafetyZone.models import Project, Config, ProjectConfig
 
-configs = ProjectConfig.objects.filter(projectID_id = 1).values()
-imageSaveLocation = configs[49]['configValue']
-serverLogLocation = configs[45]['configValue']
+# configs = ProjectConfig.objects.filter(projectID_id = 1).values()
+# imageSaveLocation = configs[49]['configValue']
+# serverLogLocation = configs[45]['configValue']
 
-
+rois_path = "C:/Users/Harun/Desktop/ObjectDetection_app/SafetyZone/rois.json"
 #to capture video class
 class VideoCamera(object):
     def __init__(self):
@@ -65,7 +66,7 @@ def gen2(camera):
 
 
 def draw_polly_and_check_isin(image, boxes, scores, classes, isIn):
-    class_id, poly_point_list, is_object_have, reverse = rois.get_rois("C:/Users/Harun/Desktop/ObjectDetection_app/SafetyZone/rois.json")
+    class_id, poly_point_list, is_object_have, reverse = rois.get_rois(rois_path)
     # print(poly_point_list)
     pts = np.array(poly_point_list[0], np.int32)
     pts = pts.reshape((-1, 1, 2))
@@ -79,12 +80,39 @@ def draw_polly_and_check_isin(image, boxes, scores, classes, isIn):
     boxes = np.squeeze(boxes)
     scores = np.squeeze(scores)
     classes = np.squeeze(classes).astype(np.int32)
-    # x = image.shape[0]
-    # y = image.shape[1]
-    # object_detection_folder_onnxTF2.check_rois()
-    # for i in range(min(1, a.shape[0])):
-    #     if b is None or b[i] > 0.7:
-    #         box = tuple(a[i].tolist())
+    x = image.shape[0]
+    y = image.shape[1]
+    # print(pts)
+    for i in range(min(1, boxes.shape[0])):
+        if scores is None or scores[i] > 0.7:
+            box = tuple(boxes[i].tolist())
+            print(check_rois(image, poly_point_list[0], box, "middlecenter"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     #         startpoint = (int(y*box[1]), int(x*box[0]))
     #         endpoint = (int(y*box[3]), int(x*box[2]))
     #         # [[1900, 1079], [1900, 70], [150, 70], [150, 1079]]
@@ -97,26 +125,26 @@ def draw_polly_and_check_isin(image, boxes, scores, classes, isIn):
     #             if isIn == False:
     #                 image = cv2.putText(image, 'NOK', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
 
-    if (isIn == True):
-            image_name = str(datetime.datetime.now()).replace(" ", "_").replace(".",":") + '_detected' +'.jpg'
-            image_name_orj = str(datetime.datetime.now()).replace(" ", "_").replace(".",":") +'.jpg'
-            # cv2.imwrite(os.path.join(imageSaveLocation, image_name_orj), image_orj)
-            # cv2.imwrite(os.path.join(imageSaveLocation, image_name), image_detected)
-            # print(os.path.join(configs[50]['configValue'], image_name), 'olarak kaydedildi')
-            path = str(datetime.date.today()) + '.txt'
-            direction =  os.path.join(str(serverLogLocation), path)
-            if not os.path.exists(direction):
-                with open( direction , 'w+') as f:
-                    f.write(str(os.path.join(imageSaveLocation, image_name_orj)) + ' olarak kaydedildi')
-                    f.write('\n')
-                    f.write(str(os.path.join(imageSaveLocation, image_name)) + ' olarak kaydedildi')
-                    f.write('\n')
-                    f.close()
-            else:
-                with open( direction , 'a') as f:
-                    f.write(str(os.path.join(imageSaveLocation, image_name_orj)) + ' olarak kaydedildi')
-                    f.write('\n')
-                    f.write(str(os.path.join(imageSaveLocation, image_name)) + ' olarak kaydedildi')
-                    f.write('\n')
-                    f.close()
+    # if (isIn == True):
+    #         image_name = str(datetime.datetime.now()).replace(" ", "_").replace(".",":") + '_detected' +'.jpg'
+    #         image_name_orj = str(datetime.datetime.now()).replace(" ", "_").replace(".",":") +'.jpg'
+    #         # cv2.imwrite(os.path.join(imageSaveLocation, image_name_orj), image_orj)
+    #         # cv2.imwrite(os.path.join(imageSaveLocation, image_name), image_detected)
+    #         # print(os.path.join(configs[50]['configValue'], image_name), 'olarak kaydedildi')
+    #         path = str(datetime.date.today()) + '.txt'
+    #         direction =  os.path.join(str(serverLogLocation), path)
+    #         if not os.path.exists(direction):
+    #             with open( direction , 'w+') as f:
+    #                 f.write(str(os.path.join(imageSaveLocation, image_name_orj)) + ' olarak kaydedildi')
+    #                 f.write('\n')
+    #                 f.write(str(os.path.join(imageSaveLocation, image_name)) + ' olarak kaydedildi')
+    #                 f.write('\n')
+    #                 f.close()
+    #         else:
+    #             with open( direction , 'a') as f:
+    #                 f.write(str(os.path.join(imageSaveLocation, image_name_orj)) + ' olarak kaydedildi')
+    #                 f.write('\n')
+    #                 f.write(str(os.path.join(imageSaveLocation, image_name)) + ' olarak kaydedildi')
+    #                 f.write('\n')
+    #                 f.close()
     return image
--- SafetyZone/views.py
+++ SafetyZone/views.py
@@ -1,3 +1,4 @@
+from asyncio import tasks
 import re
 from django.shortcuts import render
 from datetime import date, datetime,timedelta
@@ -7,17 +8,19 @@ from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 import json
 from SafetyZone.camera import VideoCamera,gen, gen2
+import requests
+import cv2
 
 from SafetyZone.models import Project, Config, ProjectConfig
 from SafetyZone.serializers import ProjectSerializer,ConfigSerializer,ProjectConfigSerializer
+from celery import shared_task
 
-
+# videocamera = cv2.VideoCapture(0)
 cam = VideoCamera() 
 
 @csrf_exempt
 def video_feed(request):
-    # JsonResponse("asd", safe=False)
-	return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
 
 @csrf_exempt
 def video_feed_object_detection(request):
@@ -30,3 +33,7 @@ def get_projects(request, id = 0):
         projects_serializer = ProjectSerializer(projects, many = True)
         return JsonResponse(projects_serializer.data, safe=False)
     # return JsonResponse("asd", safe=False)
+# requests.get('http://127.0.0.1:8000/video_feed')
+# requests.get('http://127.0.0.1:8000/object_detection')
+# threading.Thread(target=video_feed, args=(1), daemon=True).start()
+# threading.Thread(target=video_feed_object_detection, args=(1), daemon=True).start()
