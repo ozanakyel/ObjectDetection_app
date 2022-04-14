46a55dc10d6c566a2dc1976e1be363da37fba257
HarunResitKarahan <49417962+HarunResitKarahan@users.noreply.github.com>
2022-04-14 12:30:21 +0300

    Delete SafetyZone directory

--- SafetyZone/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import re_path
-
-urlpatterns = [
-    re_path('get_projects', views.get_projects),
-    re_path('video_feed', views.video_feed),
-    re_path('object_detection', views.video_feed_object_detection)
-]
