65a89f417e035f0e83e387aa4b3081eb57754d82
Harun <harun_artin@hotmail.com>
2022-04-14 23:13:57 +0300

    .

--- ObjectDetection_app-main/SafetyZone/object_detector.py
+++ ObjectDetection_app-main/SafetyZone/object_detector.py
@@ -142,18 +142,8 @@ class ObjectDetection(object):
 
             return image, boxes, scores, classes
         elif model_name == 'Mask-Rcnn':
-            detection_graph = tf.Graph()
-            with detection_graph.as_default():
-                od_graph_def = tf.GraphDef()
-                with tf.gfile.GFile(self.PATH_TO_CKPT, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tf.import_graph_def(od_graph_def, name='')
             category_index = label_map_util.create_category_index_from_labelmap(self.PATH_TO_LABELS, use_display_name=True)
-            # def load_image_into_numpy_array(image):
-            #     (im_width, im_height) = image.size
-            #     return np.array(image.getdata()).reshape(
-            #     (im_height, im_width, 3)).astype(np.uint8)
+
             def run_inference_for_single_image(image, graph):
                 with graph.as_default():
                     with tf.Session() as sess:
@@ -203,9 +193,8 @@ class ObjectDetection(object):
             # image_np = load_image_into_numpy_array(image)
             # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image_np_expanded = np.expand_dims(image_rgb, axis=0)
             # Actual detection.
-            output_dict = run_inference_for_single_image(image, detection_graph)
+            output_dict = run_inference_for_single_image(image, self.detection_graph)
             # Visualization of the results of a detection.
             vis_util.visualize_boxes_and_labels_on_image_array(
                 image,
