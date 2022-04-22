6689de44a1de4596bd23b1090d26cffeffd6d7cd
Harun <harun_artin@hotmail.com>
2022-04-22 16:51:10 +0300

    .

--- ObjectDetection_Front-End/src/components/Home/Homepage.vue
+++ ObjectDetection_Front-End/src/components/Home/Homepage.vue
@@ -36,20 +36,30 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
                     <span>Proje Ekle</span>
                 </div>
-                <div class="input">
-                  <label for="pname">Proje Adı:</label>
-                  <input type="text" id="pname" name="lname">
-                </div>
-                <div class="input">
-                  <label for="pname">Proje Adı:</label>
-                  <input type="text" id="pname" name="lname">
-                </div>
-                <div class="input">
-                  <label for="pname">Proje Adı:</label>
-                  <input type="text" id="pname" name="lname">
+                <div class="inputs">
+                  <div class="input">
+                    <label for="pname">Proje Adı:</label>
+                    <input type="text" id="pname" name="lname">
+                  </div>
+                  <div class="input">
+                    <label for="pname">Proje Adı:</label>
+                    <input type="text" id="pname" name="lname">
+                  </div>
+                  <div class="input">
+                    <label for="pname">Proje Adı:</label>
+                    <input type="text" id="pname" name="lname">
+                  </div>
+                  <div class="input">
+                    <label for="pname">Proje Adııııı:</label>
+                    <input type="text" id="pname" name="lname">
+                  </div>
+                  <div class="input">
+                    <label for="pname">Proje Adı:</label>
+                    <input type="text" id="pname" name="lname">
+                  </div>
                 </div>
                 <div class="send kaydet">
-                    <p>KAYDET</p>
+                    <p>EKLE</p>
                 </div>
               </div>
             </div>
